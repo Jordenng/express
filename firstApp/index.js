624265e30beb11981d5d0a0427464b682baa3dda
Yarden Nagel <nagel.yarden@gmail.com>
2021-07-29 15:14:58 +0300

    inital commit

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-    console.log("We got a new request!");
-});
+// app.use((req, res) => {
+//     console.log("We got a new request!");
+//     // res.send("helloo")
+//     res.send('<h1>this is my webpage</h1>')
+// });
+app.get("/",(req, res)=>{
+    res.send("this is home page");
+})
+app.get('/cats',(req,res)=>{
+    res.send("cat response");
+})
 
+app.get("/dogs",(req,res)=>{
+    res.send('woof');
+})
+
+app.get("*", (req,res)=>{
+    res.send("i dont know this route");
+})
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
